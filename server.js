f112bff0577a37ef1160440511eca41d5188c5c7
Aakash Verma <aakashvermaee@gmail.com>
2019-09-16 15:01:00 +0530

    initial heroku deployment works

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+const { PORT = 4200, HOST = '0.0.0.0' } = process.env;
+
+const dirToServe = path.join(__dirname, 'dist/MedicSoft-App');
+
+app.use(express.static(dirToServe));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(dirToServe + '/index.html');
+});
+
+app.listen(PORT, HOST, (err) => {
+  if (err) throw err;
+
+  console.log(`Webclient Running on http://${HOST}:${PORT}`);
+});
